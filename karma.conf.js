ed3911a68bf16c74343ffc5ea7329ac67e88d366
daeniz <dennis@schmock.eu>
2016-12-22 02:46:15 +0100

    rearranged bower

--- karma.conf.js
+++ karma.conf.js
@@ -11,10 +11,9 @@ module.exports = function (config) {
             'bower_components/angular-mocks/angular-mocks.js',
             'bower_components/angular-animate/angular-animate.js',
             'bower_components/angular-bootstrap/ui-bootstrap.js',
-            'bower_components/angular-bootstrap/ui-bootstrap.js',
             'bower_components/angularfire/dist/angularfire.js',
-            'bow*/**/*.js',
-            'bow*/**/**/.js',
+
+
             'app.js',
 
 
