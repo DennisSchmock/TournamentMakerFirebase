c5f58a7a2c713148d22925e5e59ec79c02e83b22
daeniz <dennis@schmock.eu>
2016-12-22 03:08:46 +0100

    rearranged bower

--- karma.conf.js
+++ karma.conf.js
@@ -12,14 +12,14 @@ module.exports = function (config) {
             'bower_components/angular-animate/angular-animate.js',
             'bower_components/angular-bootstrap/ui-bootstrap.js',
             'bower_components/angularfire/dist/angularfire.js',
-
-
-            'app.js',
-
-
             'components/*.js',
             'Tournaments/*.js',
-            'Players/*.js'
+            'Players/*.js',
+
+            'app.js'
+
+
+
         ],
 
         autoWatch: true,
